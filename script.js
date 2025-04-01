f0ab5655527de4eb825848a7fe2924e4ca181d8e
MusaeM <mustafa.marouf@hotmail.com>
2025-04-01 13:01:09 +0200

    Change scanner resolution back

--- script.js
+++ script.js
@@ -15,16 +15,12 @@ document.getElementById("scan-btn").addEventListener("click", () => {
     video.srcObject?.getTracks()?.forEach(track => track.stop());
 
     navigator.mediaDevices.getUserMedia({
-        video: { 
-      facingMode: "environment",
-      width: {ideal: 1280},
-      height: {ideal: 720}
-  },
-        audio: false
-      }).then((stream) => {
-        video.srcObject = stream;
-        video.play();
-        
+      video: { facingMode: "environment" },
+      audio: false
+    }).then((stream) => {
+      video.srcObject = stream;
+      video.play();
+
       let detectionHistory = [];
 
       Quagga.init({
