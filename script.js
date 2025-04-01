0df63f4977b2622ff0fb94a283c27b29a56855fe
MusaeM <mustafa.marouf@hotmail.com>
2025-04-01 14:22:26 +0200

    Go backfrom test high res

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ function startScanner() {
   navigator.mediaDevices.getUserMedia({
     video: {
       facingMode: "environment",
-      width: { min: 640, ideal: 1920, max: 1920 },
-      height: { min: 480, ideal: 1080, max: 1080 }
+      width: { min: 640, ideal: 1280, max: 1920 },
+      height: { min: 480, ideal: 720, max: 1080 }
     },
     audio: false
   })
